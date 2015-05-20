10f29a96ef7b0d5f4d6b4f596484a9a254330036
Marcin Słowiński <mslowinski79@gmail.com>
2015-05-20 15:06:06 +0200

    remains ten to complete

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,26 +1,49 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn boolean
+  [x]
+  (if (or (nil? x) (false? x))
+    false
+    true))
 
-(defn abs [x]
-  ":(")
+(defn abs
+  [x]
+  (if (< x 0)
+    (- x)
+    x))
 
-(defn divides? [divisor n]
-  ":(")
+(defn divides?
+  [divisor n]
+  (if (= (mod n divisor) 0)
+    true
+    false))
 
-(defn fizzbuzz [n]
-  ":(")
+(defn fizzbuzz
+  [n]
+  (cond
+   (= (mod n 15) 0) "gotcha!"
+   (= (mod n 3) 0) "fizz"
+   (= (mod n 5) 0) "buzz"
+   :else ""
+   ))
 
 (defn teen? [age]
-  ":(")
+  (<= 13 age 19))
 
 (defn not-teen? [age]
-  ":(")
+  (or (< age 13) (> age 19)))
+
+   
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* x 2) 
+    (empty? x) nil
+    (list? x) (* (count x) 2)
+    (vector? x) (* (count x) 2)
+    :else true
+  ))
 
 (defn leap-year? [year]
   ":(")
